5a4899ce5c817940d991ae1a821828c5f71ba9ad
Roy Kensmil <rkensmil2@gmail.com>
2024-07-27 18:39:10 +0000

    Test github actions

--- test_hello.py
+++ test_hello.py
@@ -2,11 +2,11 @@ from hello import toyou, add, subtract
 
 def setup_function(function):
     print("Running Setup: %s" % function.__name__)
-    function.x = 10
+    function.x = 11
 
 def teardown_function(function):
     print("Running Teardown: %s" % function.__name__)
     del function.x
 
 def test_hello_subtract():
-    assert subtract(test_hello_subtract.x) == 9
+    assert subtract(test_hello_subtract.x) == 10
